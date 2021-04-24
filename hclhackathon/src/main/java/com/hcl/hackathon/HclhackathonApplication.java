1b35a59d3f75af5d84d7fe71e484db551d31ec62
krishnakanthctr <42808711+krishnakanthctr@users.noreply.github.com>
2021-04-24 11:05:55 +0530

    Add files via upload

---
+++ hclhackathon/src/main/java/com/hcl/hackathon/HclhackathonApplication.java
@@ -0,0 +1,13 @@
+package com.hcl.hackathon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HclhackathonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HclhackathonApplication.class, args);
+	}
+
+}
